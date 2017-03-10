9d6163334a488225bf622d19abc853857a6e9c31
Stephan Dubuke <sdubuke@csumb.edu>
2017-03-10 18:42:34 +0000

    updates to login function on app.py page 3-10-17

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import flask, os
 from flask import render_template, flash, request
 from flask_wtf import Form
-from wtforms import TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import TextField, TextAreaField, validators, StringField, SubmitField, csrf
 from slackclient import SlackClient
+
+from hashlib import md5
+
 
 app = flask.Flask(__name__)
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    # email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
+
+SECRET_KEY = '1234567890'
+
+# class IPAddressCSRF(csrf):
+#     """
+#     Generate a CSRF token based on the user's IP. I am probably not very
+#     secure, so don't use me.
+#     """
+#     def setup_form(self, form):
+#         self.csrf_context = form.meta.csrf_context
+#         return super(IPAddressCSRF, self).setup_form(form)
+
+#     def generate_csrf_token(self, csrf_token):
+#         token = md5(SECRET_KEY + self.csrf_context).hexdigest()
+#         return token
+
+#     def validate_csrf_token(self, form, field):
+#         if field.data != field.current_token:
+#             raise ValueError('Invalid CSRF')
+
+class LoginForm(Form):
+    username = TextField(validators=[validators.required()])
+    password = TextField(validators=[validators.required(), validators.Length(min=3, max=35)])
 
 @app.route('/')
 def index():
@@ -21,21 +43,23 @@ def index():
 # If there is no userName, then route to loginScreen. Else, route to the main page.
     return flask.render_template("index.html")
     
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
 # If there is no userName, then route to loginScreen. Else, route to the main page.
-    form = ReusableForm(request.form)
- 
+    return flask.render_template("login.html")
+    
+@app.route('/loginDB', methods=['GET', 'POST'])
+def loginBD():
+    form = LoginForm(request.form)
     print (form.errors)
     if request.method == 'POST':
         if request.form['submit'] == 'Login':
-            name=request.form['userName']
+            username=request.form['userName']
             password=request.form['userPassword']
-            print (name, " ", password)
- 
+            print (username, " ", password)
+            return flask.render_template("login.html", form=form)
         if form.validate():
-            # Save the comment here.
-            flash('Thanks for registration ' + name)
+            flash('Thanks for registration ' + username)
         elif request.form['submit'] == 'Create Account':
             return render_template('createaccount.html', form=form)
     return flask.render_template("login.html")
