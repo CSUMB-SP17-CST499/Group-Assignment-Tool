e3514ed975b8ae499a44b70174853e8fc78b7a0e
Michael <micfernandez@csumb.edu>
2017-03-17 04:11:50 +0000

    circleCI initial setup

--- apis/slack.py
+++ apis/slack.py
@@ -19,6 +19,24 @@ def get_user_groups():
        names.append(data['usergroups'][i]['name'])
     return names
     
+def update_users():
+    """Function creates a 'get'reques by passing the token as a parameter for authentication.
+
+    Args:
+        none
+
+    Returns:
+        Returns a list containing Slack group names.
+
+    """
+    request = requests.get('https://slack.com/api/usergroups.list', params = {'token': token.get_slack_token(), 'include_users': True})
+    data = request.json()
+    names = []
+    size = len(data)
+    for i in range (0, 2):
+       names.append(data['usergroups'][i]['name'])
+    return names
+    
 def update_employees():
     pass
 
