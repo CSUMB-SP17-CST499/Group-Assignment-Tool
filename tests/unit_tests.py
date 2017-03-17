3bb7a6ea2134ad22f99b3722b6bed99519b67142
Michael <micfernandez@csumb.edu>
2017-03-17 06:11:08 +0000

    unit_tests for slack.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,12 +1,36 @@
 import unittest
-import app
+from apis import slack
 
-class RenderTests(unittest.TestCase):
-    render_templates = False
-    
-    def test_empty_string(self):
+class testSlackFunctions(unittest.TestCase):
+
+    def test_self(self):
         result = ''.split()
         self.assertEquals(result, [])
+        
+    def test_get_user_groups(self):
+        result = slack.get_user_groups()
+        self.assertIsNotNone(result)
+        print(result)
+        
+    def test_get_users(self):
+        groups = slack.get_user_groups()
+        for x in groups:
+            result = slack.get_users(x)
+            self.assertIsNotNone(result)
+            print(result)
+            
+    def test_get_user_names(self):
+        groups = slack.get_user_groups()
+        result = slack.get_users(groups[0])
+        # userName = slack.get_user_names(result)
+        # print(userName)
+            
+    def test_update_employees(self):
+        slack.update_employees()
+    
+    def test_create_group(self):
+        slack.create_group()
+
 
 if __name__ == '__main__':
     unittest.main()
