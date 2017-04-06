55f26d4016be97d97cf08bad60caaf13e0a14fd7
gand4052 <elgandara@csumb.edu>
2017-04-06 05:42:43 +0000

    Add the get_instance_by_field to db/query.py functions

--- db/encode.py
+++ db/encode.py
@@ -5,16 +5,24 @@ import json
 
 def get_json(name, value, exclude = []):
     obj = None
-    
     if isinstance(value, Model):
         obj = value.get_dict(exclude)
     elif isinstance(value, list):
         obj = [val.get_dict(exclude) for val in value]
     else:
         raise TypeError("get_json() recieved unexpected type for argument",
-                        "'%s: '%s'" % ("value", type(value)) )
+                        "%s: %s" % ("value", type(value)) )
         
     return json.dumps({name: obj})
+
+
+def create_error(error, exception = None):
+    response = {'error': error}
+    
+    if exception:
+        response['message'] = str(exception)
+        
+    return json.dumps(response)
 
 
 def get_employees_json():
