ce10b4f0c234fd09f29a2bb9bec43c38c333c1ee
gand4052 <elgandara@csumb.edu>
2017-04-22 21:58:52 +0000

    Updated the add employee page and js

--- dashboard/static/js/AddEmployee.js
+++ dashboard/static/js/AddEmployee.js
@@ -6,20 +6,18 @@ $(document).ready( function() {
     });
     
     $('#create_user').on('click',function(e) {
-        console.log('I clicked a button');
-    
         var firstname = $('#first_name').val();
         var lastname = $('#last_name').val();
         var email = $('#email').val();
-        var role = $('#Role').val();
-        
+        var roles = [$('#roles').val()];
+
         data = {
             'first_name': firstname, 
             'last_name': lastname,
             'email': email, 
-            'role': role
+            'roles': roles
         };
-                
+        
         $.ajax({
             url: '/api/employee',
             method: 'PUT',
@@ -29,18 +27,27 @@ $(document).ready( function() {
                  $('#message').html("User was created");
                  $('#alert-message')[0].classList.add('alert-success');
                  $('#alert-message').show();
-                 window.location = '/employees';
-                 // refresh the page so that the valeus are not still on the page after the user was created
-                 //document.url;
             },
-            error: function(error) {
+            error: function(response) {
                 try {
-                    json = JSON.parse(error.responseText);
-                    if (json.message) {
-                        $('#message').html(json.message);
+                    json = JSON.parse(response.responseText);
+                    error = json.error;
+                    if (error == 'email_taken') {
+                        $('#message').html('The provided email is already taken.');
                         $('#alert-message')[0].classList.add('alert-danger');
                         $('#alert-message').show();
                     }
+                    else if (error == 'unexpected_error') {
+                        $('#message').html('There was an unexpected error. Please contact the administrator.');
+                        $('#alert-message')[0].classList.add('alert-danger');
+                        $('#alert-message').show();
+                    }
+                    else if (error == 'missing_arguments') {
+                        $('#message').html('There are missing fields.');
+                        $('#alert-message')[0].classList.add('alert-danger');
+                        $('#alert-message').show();
+                    }
+                    console.log(response);
                 }
                 catch (e) {
                     console.log(e);
