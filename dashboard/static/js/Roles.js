8a952fa745ee460728fda9731439c0387bbf4ac6
Michael <micfernandez@csumb.edu>
2017-04-27 05:49:21 +0000

    add IDs to checkboxes in Roles table

--- dashboard/static/js/Roles.js
+++ dashboard/static/js/Roles.js
@@ -12,8 +12,9 @@ $(document).ready(function(){
         success: function(response) {
             json = (JSON.parse(response))['roles'];
             tableRows = Object.keys(json).length;
-            loadTable(table, tableRows,4);
+            loadRolesTable(table, tableRows,4);
             displayRoles(table, json);
+            console.log(json[1]['id']);
         },
         error: function(error) {
             try {
@@ -30,15 +31,75 @@ $(document).ready(function(){
         }
     });
     
+    function loadRolesTable(table, tableRows, columnAmt) {
+    var column_amt = columnAmt;
+    var inner_table = "";
+    var id, name, description;
+    inner_table += "<tbody>"
+
+    for (var row = 0; row < tableRows; row++){
+        id = json[row]["id"];
+        inner_table += "<tr>";
+        inner_table += "<td><input class='checkbox' type='checkbox' value='" + id + "' id='" + id + "' /></td>";
+        inner_table += "<td></td>";
+        inner_table += "<td></td>";
+        inner_table += "<td></td>";
+        inner_table += "</tr>";
+    };
+    inner_table += "</tbody>";
+    
+    $(table).append(inner_table);
+    
+    $('#deleteButton').click(function(){
+        var peopleToDelete = [];
+        
+        $('.checkbox:checkbox:checked').each(function() {
+            peopleToDelete.push($(this).val());
+        });
+        
+        data = {
+            'id': peopleToDelete,
+        }
+        
+        console.log(peopleToDelete);
+        
+        $.ajax({
+            url: '/api/employee',
+            method: 'DELETE',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: function(response) {
+                $('#message').html("User(s) deleted");
+                $('#alert-message')[0].classList.add('alert-success');
+                $('#alert-message').show();
+                window.location = "/employees";
+            },
+            error: function(error) {
+                try {
+                    json = JSON.parse(error.responseText);
+                    if (json.message) {
+                        $('#message').html(json.message);
+                        $('#alert-message')[0].classList.add('alert-danger');
+                        $('#alert-message').show();
+                    }
+                }
+                catch (e) {
+                    console.log(e);
+                }
+            }
+        });
+    });
+}
+    
     function displayRoles(table, roles) {
         if (roles) {
             
             for (var roles_index = 0; roles_index < roles.length; roles_index++) {
                 
                 var role = roles[roles_index];
-                
-                console.log(role["name"])
-                table.rows[roles_index + 1].cells[1].innerHTML = role["name"]//name
+                console.log(role);
+                console.log(role["name"]);
+                table.rows[roles_index + 1].cells[1].innerHTML = role["name"]; //name
                 table.rows[roles_index + 1].cells[2].innerHTML = role['description'];//description
                 
                 var roles_cell = table.rows[roles_index + 1].cells[3]
