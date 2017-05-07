487e314ba8d46bd6525b1e6aac0d54cd233a7764
gand4052 <elgandara@csumb.edu>
2017-05-07 20:50:01 +0000

    Updated a method name in the slack.py file and the corresponding slack_test.py method test

--- apis/slack.py
+++ apis/slack.py
@@ -142,7 +142,9 @@ def update_usergroup_users(user_group_id, user_ids):
     data = []
     try:
         request = requests.get('https://slack.com/api/usergroups.users.update', 
-                params = {'token': token.get_slack_token(), 'usergroup': user_group_id, 'users': user_ids},
+                params = {'token': token.get_slack_token(), 
+                        'usergroup': user_group_id, 
+                        'users': user_ids},
                 headers = {'Connection': 'close'} )
                 
         data = request.json()
@@ -151,7 +153,6 @@ def update_usergroup_users(user_group_id, user_ids):
         print(e)
             
     return data
-    
     
 
 def get_user_list():
@@ -166,7 +167,6 @@ def get_user_list():
 
     """
     data = []
-    
     try:
         request = requests.get('https://slack.com/api/users.list', 
                 params = {'token': token.get_slack_token()},
@@ -178,6 +178,7 @@ def get_user_list():
         print(e)
     
     return data
+    
     
 def get_user_groups_list():
     """Function creates a 'get'request, in order to get all the information from the groups in the Slack channel,
@@ -191,7 +192,6 @@ def get_user_groups_list():
 
     """
     data = []
-    
     try:
         request = requests.get('https://slack.com/api/usergroups.list', 
                 params = {'token': token.get_slack_token()},
@@ -204,3 +204,29 @@ def get_user_groups_list():
     
     return data
     
+
+def get_usergroup_users_list(usergroup_id):
+    """Returns a list with the ids of the users in a usergroup.
+    
+    Args:
+        usergroup_id: The id of a Slack usergroup.
+        
+    Returns:
+        Returns a list containing the ids of the users in the
+        specified usergroup.
+    """
+    user_ids = []
+    try:
+        request = requests.get('https://slack.com/api/usergroups.users.list', 
+                params = {'token': token.get_slack_token(),
+                        'usergroup': usergroup_id },
+                headers = {'Connection': 'close'})
+        
+        data = request.json()
+        if data.get('ok'):
+            user_ids = data.get('users')
+        
+    except Exception as e:
+        print(e)
+        
+    return user_ids
