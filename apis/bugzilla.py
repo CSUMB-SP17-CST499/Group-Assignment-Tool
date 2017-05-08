b4b17a0b723a9975bbdb1685d182d308abbf0d35
Michael <micfernandez@csumb.edu>
2017-05-08 02:31:00 +0000

    working on adding user to bugzilla group

--- apis/bugzilla.py
+++ apis/bugzilla.py
@@ -1,4 +1,4 @@
-import os, requests
+import os, requests, json
 
 def get_bugzilla_user_info(user):
     """Function used to obtain bugzilla user info
@@ -53,3 +53,31 @@ def get_all_bugzilla_group_info():
         'Bugzilla_password': os.getenv('Bugzilla_password')}
         )
     return(request.json())
+    
+def add_user_to_bugzilla_group(user):
+    """Function used to obtain bugzilla user info
+
+    Args:
+        param1 (str): either a buzilla user's login name/email or id
+        param2 (list: str) an array of either group names or ids
+        
+
+    Returns:
+        Returns id, name, real_name, email, can_login, and groups of user
+
+    """
+    
+    data = {}
+    data['set'] = ['test']
+    wrapData = {}
+    wrapData['groups'] = data
+    
+    
+    print(wrapData)
+    # request = requests.put('http://www.fruitfuldevelopment.com:8081/bugzilla/rest.cgi/user/' + user,
+    #     params = {'Bugzilla_login': os.getenv("Bugzilla_login"),
+    #     'Bugzilla_password': os.getenv('Bugzilla_password'), 
+    #     'groups': data}
+    #     )
+    # return(request.json())
+ 
